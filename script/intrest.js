fa695e3170d3bcc428d0d8045c8b1b4fe99db8a6
Alex <alxmer111@gmail.com>
2018-04-18 22:10:23 +0800

    commit

--- script/intrest.js
+++ script/intrest.js
@@ -5,6 +5,5 @@ addInterestButton.addEventListener(
     if (extractedInterest) {
       var html = "<button class='removable' onclick='this.remove()'>"+ extractedInterest +"</button>";
       target.innerHTML += html;
-      target.addEventListener('click', target.remove);
     } else { console.warn('input is empty') }    
   });
