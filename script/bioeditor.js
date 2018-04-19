944c58dc14b65fd97139e7a3cd7830a44c19b14c
Alex <alxmer111@gmail.com>
2018-04-20 01:43:50 +0800

    added localstorage

--- script/bioeditor.js
+++ script/bioeditor.js
@@ -1,13 +1,56 @@
-var text = document.getElementById('name').textContent;
-document.getElementById('name').addEventListener(
-    'click',
-    function(event) {
-      var newText = prompt('Новое имя', text);
-      if (newText) {
-      document.getElementById("name").textContent = newText;
-      }
-      else {
-        console.log('input is empty');
-      }
-    }
-  );
+//name edit
+if (localStorage.name) {
+  document.getElementById("nameChanger").value = localStorage.name;
+}
+
+document.getElementById("nameChangerForm").addEventListener("submit", function(event){
+  //prevent using Enter as submit button
+  event.preventDefault();                                  
+})
+
+document.getElementById("nameChanger").addEventListener("blur", function(){
+  var newName = document.getElementById("nameChanger").value;
+  localStorage.setItem("name", newName);
+})
+
+//phone edit
+if (localStorage.phone) {
+  document.getElementById("phoneChanger").value = localStorage.phone;
+}
+
+document.getElementById("phoneChangerForm").addEventListener("submit", function(event){  
+  event.preventDefault();                                  
+})
+
+document.getElementById("phoneChanger").addEventListener("blur", function(){
+  var newPhone = document.getElementById("phoneChanger").value;
+  localStorage.setItem("phone", newPhone);
+})
+
+//email edit
+if (localStorage.email) {
+  document.getElementById("emailChanger").value = localStorage.email;
+}
+
+document.getElementById("emailChangerForm").addEventListener("submit", function(event){  
+  event.preventDefault();                                  
+})
+
+document.getElementById("emailChanger").addEventListener("blur", function(){
+  var newEmail = document.getElementById("emailChanger").value;
+  localStorage.setItem("email", newEmail);
+})
+//relationship edit
+var select = document.querySelector("#count");
+var selectOption = select.options[select.selectedIndex];
+var lastSelected = localStorage.getItem('select');
+
+if(lastSelected) {
+    select.value = lastSelected; 
+}
+
+select.onchange = function () {
+   lastSelected = select.options[select.selectedIndex].value;
+   console.log(lastSelected);
+   localStorage.setItem('select', lastSelected);
+}
